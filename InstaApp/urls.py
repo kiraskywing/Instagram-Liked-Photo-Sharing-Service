eee49464c0e6fd83f9ce9991fbac185af7f8ef2c
Yi-Chang Lin <kiraskywing@gmail.com>
2021-08-06 13:56:42 +0800

    Add post_detail function

--- InstaApp/urls.py
+++ InstaApp/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from InstaApp.views import HelloWorld, PostsView
+from InstaApp.views import HelloWorld, PostsView, PostDetailView
 
 urlpatterns = [
     path('', HelloWorld.as_view(), name='helloworld'),
     path('posts/', PostsView.as_view(), name='posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
 ]
--- InstaApp/views.py
+++ InstaApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 
 from InstaApp.models import Post
 
@@ -10,3 +10,7 @@ class HelloWorld(TemplateView):
 class PostsView(ListView):
     model = Post
     template_name = 'index.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'

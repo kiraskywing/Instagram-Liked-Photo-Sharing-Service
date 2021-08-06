236b581ab18ef6c7b4c3ef9c5c0189209f308b10
Yi-Chang Lin <kiraskywing@gmail.com>
2021-08-06 20:55:56 +0800

    Add customizedUser

--- InstaApp/views.py
+++ InstaApp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from InstaApp.models import Post
+from InstaApp.forms import CustomUserCreationForm
 
 # Create your views here.
 class HelloWorld(TemplateView):
@@ -17,10 +19,11 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_create.html'
     fields = '__all__'
+    login_url = 'login'
 
 class PostUpdateView(UpdateView):
     model = Post
@@ -31,3 +34,8 @@ class PostDeleteView(DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy("posts")
+
+class SignUp(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy("login")

b98f6684291a1b56092edfb7895f88f3e68a7c38
Yi-Chang Lin <kiraskywing@gmail.com>
2022-07-14 16:19:16 +0800

    prepare for deployment

--- SimpleInstagram/settings.py
+++ SimpleInstagram/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^+tyter&(^-k89ux=%n=)25a5h_6j=ca_pty0ezjp9jc6f8870
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'InstaApp',
     'imagekit',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
